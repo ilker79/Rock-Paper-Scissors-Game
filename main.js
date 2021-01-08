bbfd8db57e019286463a2c6b340a8c3795ebaf43
ilker <ilkerpeker79@yahoo.com>
2021-01-08 11:49:14 +0000

    rest is yours

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-let playerMove
-let computerMove 
+let playerMove;
+let computerMove;
+
+let playerChoice = prompt("What is your choice?");
 
 function getWinner(playerMove, computerMove) {
   if (playerMove === "Rock" && computerMove === "Rock") {
-    return '0';
+    return "0";
   } else if (playerMove === "Rock" && computerMove === "Scissors") {
-    return '1';
+    return "1";
   } else if (playerMove === "Rock" && computerMove === "Paper") {
-    return '-1';
+    return "-1";
   } else if (playerMove === "Paper" && computerMove === "Rock") {
-    return '1';
+    return "1";
   } else if (playerMove === "Paper" && computerMove === "Scissors") {
-    return '-1';
+    return "-1";
   } else if (playerMove === "Paper" && computerMove === "Paper") {
-    return '0';
+    return "0";
   } else if (playerMove === "Scissors" && computerMove === "Rock") {
-    return '-1';
+    return "-1";
   } else if (playerMove === "Scissors" && computerMove === "Scissors") {
-    return '0';
+    return "0";
   } else if (playerMove === "Scissors" && computerMove === "Paper") {
-    return '1';
+    return "1";
   }
 }
 
-let result = getWinner();
+let result = getWinner(playerChoice, "Paper");
+console.log(result);
