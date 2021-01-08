73781e5cb590f7eaedd92a6e03b7720e45fcc71b
Ellie <ellieredmayne@hotmail.com>
2021-01-08 10:41:00 +0000

    Update main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+let playerMove = "Rock";
+let computerMove = "Rock";
+
+let playerMove2 = "Paper";
+let computerMove2 = "Paper";
+
+let playerMove3 = "Scissors";
+let computerMove3 = "Scissors";
+
+if (playerMove === "Rock" && computerMove === "Rock") {
+    console.log("draw")
+} else if (playerMove === "Paper") {
+
+} else if (playerMove === "Scissors") {
+
+}
