95416d8ccdb1cccc4202a58f5b5bd3bf5b035e56
ilker <ilkerpeker79@yahoo.com>
2021-01-08 10:47:00 +0000

    Updated

--- main.js
+++ main.js
@@ -8,9 +8,21 @@ let playerMove3 = "Scissors";
 let computerMove3 = "Scissors";
 
 if (playerMove === "Rock" && computerMove === "Rock") {
-    console.log("draw")
-} else if (playerMove === "Paper") {
-
-} else if (playerMove === "Scissors") {
-
+  console.log("draw");
+} else if (playerMove === "Rock" && computerMove === "Scissors") {
+  console.log("Player wins");
+} else if (playerMove === "Rock" && computerMove === "Paper") {
+  console.log("Computer wins");
+} else if (playerMove === "Paper" && computerMove === "Rock") {
+  console.log("Player wins");
+} else if (playerMove === "Paper" && computerMove === "Scissors") {
+  console.log("Computer wins");
+} else if (playerMove === "Paper" && computerMove === "Paper") {
+  console.log("Draw");
+} else if (playerMove === "Paper" && computerMove === "Rock") {
+  console.log("Player wins");
+} else if (playerMove === "Paper" && computerMove === "Scissors") {
+  console.log("Computer wins");
+} else if (playerMove === "Paper" && computerMove === "Paper") {
+  console.log("Draw");
 }
