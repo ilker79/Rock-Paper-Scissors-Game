639fe12ea7b5f9ce0c8d51c6aab1efa55e003ff4
ilker <ilkerpeker79@yahoo.com>
2021-01-08 12:31:26 +0000

    update

--- main.js
+++ main.js
@@ -1,36 +1,39 @@
-// let playerMove;
-// let computerMove;
+let userCount = prompt("how many times you want to play");
 
-let playerChoice = prompt("What is your choice?");
+while (userCount !== 0) {
+  let playerChoice = prompt("What is your choice?");
 
-function getWinner(playerMove, computerMove) {
-  if (playerMove === "Rock" && computerMove === "Rock") {
-    return "0";
-  } else if (playerMove === "Rock" && computerMove === "Scissors") {
-    return "1";
-  } else if (playerMove === "Rock" && computerMove === "Paper") {
-    return "-1";
-  } else if (playerMove === "Paper" && computerMove === "Rock") {
-    return "1";
-  } else if (playerMove === "Paper" && computerMove === "Scissors") {
-    return "-1";
-  } else if (playerMove === "Paper" && computerMove === "Paper") {
-    return "0";
-  } else if (playerMove === "Scissors" && computerMove === "Rock") {
-    return "-1";
-  } else if (playerMove === "Scissors" && computerMove === "Scissors") {
-    return "0";
-  } else if (playerMove === "Scissors" && computerMove === "Paper") {
-    return "1";
+  function getWinner(playerMove, computerMove) {
+    if (playerMove === "Rock" && computerMove === "Rock") {
+      return "0";
+    } else if (playerMove === "Rock" && computerMove === "Scissors") {
+      return "1";
+    } else if (playerMove === "Rock" && computerMove === "Paper") {
+      return "-1";
+    } else if (playerMove === "Paper" && computerMove === "Rock") {
+      return "1";
+    } else if (playerMove === "Paper" && computerMove === "Scissors") {
+      return "-1";
+    } else if (playerMove === "Paper" && computerMove === "Paper") {
+      return "0";
+    } else if (playerMove === "Scissors" && computerMove === "Rock") {
+      return "-1";
+    } else if (playerMove === "Scissors" && computerMove === "Scissors") {
+      return "0";
+    } else if (playerMove === "Scissors" && computerMove === "Paper") {
+      return "1";
+    }
   }
+  let computerChoiceArray = ["Rock", "Paper", "Scissors"];
+
+  function computerMove() {
+    return Math.floor(Math.random() * 3);
+  }
+  let computerChoice = computerChoiceArray[computerMove()];
+  // console.log(computerMove());
+
+  let result = getWinner(playerChoice, computerChoice);
+  alert(`Computer choice is: ${computerChoice} ` + " " + result);
+
+  userCount--;
 }
-let computerChoiceArray = ['Rock', 'Paper', 'Scissors']
-
-function computerMove() {
-    return Math.floor(Math.random () * 3);
-}
-
-console.log(computerMove());
-
-let result = getWinner(playerChoice, computerChoiceArray[computerMove()]);
-alert(result);
