17548ee1b325370826d400d388df28636547f389
ilker <ilkerpeker79@yahoo.com>
2021-01-08 11:11:25 +0000

    update

--- main.js
+++ main.js
@@ -7,24 +7,26 @@ let computerMove2 = "Paper";
 let playerMove3 = "Scissors";
 let computerMove3 = "Scissors";
 
-if (playerMove && computerMove) {
-  console.log("draw");
-} else if (playerMove && computerMove2) {
-  console.log("Player wins");
-} else if (playerMove&& computerMove3) {
-  console.log("Computer wins");
-} else if (playerMove2 && computerMove) {
-  console.log("Player wins");
-} else if (playerMove2 && computerMove3) {
-  console.log("Computer wins");
-} else if (playerMove2 && computerMove2) {
-  console.log("Draw");
-} else if (playerMove3 && computerMove) {
-  console.log("Computer wins");
-} else if (playerMove3 && computerMove3) {
-  console.log("Draw");
-} else if (playerMove3 && computerMove2) {
-  console.log("Player Wins");
+function playerMove(player, computer) {
+  if (playerMove && computerMove) {
+    console.log("draw");
+  } else if (playerMove && computerMove2) {
+    console.log("Player wins");
+  } else if (playerMove && computerMove3) {
+    console.log("Computer wins");
+  } else if (playerMove2 && computerMove) {
+    console.log("Player wins");
+  } else if (playerMove2 && computerMove3) {
+    console.log("Computer wins");
+  } else if (playerMove2 && computerMove2) {
+    console.log("Draw");
+  } else if (playerMove3 && computerMove) {
+    console.log("Computer wins");
+  } else if (playerMove3 && computerMove3) {
+    console.log("Draw");
+  } else if (playerMove3 && computerMove2) {
+    console.log("Player Wins");
+  }
 }
 
-
+let result = playerMove("rock", "paper");
