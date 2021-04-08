fe461430c2e37de263bbfcd2be093be89fbf46a6
ilker <ilkerpeker79@yahoo.com>
2021-04-08 13:38:09 +0100

    game improvements added

--- main.js
+++ main.js
@@ -2,63 +2,174 @@ let userResponse;
 let gamesPlayed = 0;
 let playerWins = 0;
 let computerWins = 0;
-let draw = 0;
-let input;
+let draws = 0;
+let playerChoice;
+let computersChoice;
+let playersName;
 
 function selected() {
   if (document.querySelector("#Rock").checked) {
-    input = "Rock";
+    playerChoice = "Rock";
   } else if (document.querySelector("#Paper").checked) {
-    input = "Paper";
+    playerChoice = "Paper";
   } else if (document.querySelector("#Scissors").checked) {
-    input = "Scissors";
+    playerChoice = "Scissors";
   }
-  document.querySelector("p").innerText = input;
+
+  if (playerChoice !== undefined) {
+    document.querySelector(
+      "#player-choice"
+    ).innerText = `Player's choice is ${playerChoice}`;
+  } else {
+    document.querySelector("#player-choice").innerText =
+      "Please choose one of the options";
+  }
 }
 
-let myButton = document.querySelector("#buttonId1");
+let computerChoiceArray = ["Rock", "Paper", "Scissors"];
 
-myButton.addEventListener("click", selected);
+//Computer's choice
+function computerChoice() {
+  let randomNum = Math.floor(Math.random() * 3);
+  computersChoice = computerChoiceArray[randomNum];
+
+  if (playerChoice !== undefined) {
+    document.querySelector(
+      "#computer-choice"
+    ).innerText = `Computer choice is: ${computersChoice} `;
+  } else {
+    document.querySelector("#computer-choice").innerText = null;
+  }
+}
+
+//Reset the game
+function reset() {
+  playerChoice = undefined;
+  document.querySelector("#Rock").checked = false;
+  document.querySelector("#Paper").checked = false;
+  document.querySelector("#Scissors").checked = false;
+  document.querySelector("#computer-choice").innerText = null;
+  document.querySelector("#player-choice").innerText = null;
+  document.querySelector("#winner").innerText = null;
+}
+
+function result() {
+  if (playerChoice === "Rock" && computersChoice === "Rock") {
+    draws++;
+    document.querySelector("#winner").innerText = "Draw";
+    document.querySelector("#game-draw").innerText = draws;
+  } else if (playerChoice === "Rock" && computersChoice === "Scissors") {
+    playerWins++;
+    document.querySelector("#winner").innerText = "Player wins";
+    document.querySelector("#playerwin").innerText = playerWins;
+  } else if (playerChoice === "Rock" && computersChoice === "Paper") {
+    computerWins++;
+    document.querySelector("#winner").innerText = "Computer wins";
+    document.querySelector("#computerwin").innerText = computerWins;
+  } else if (playerChoice === "Paper" && computersChoice === "Rock") {
+    playerWins++;
+    document.querySelector("#winner").innerText = "Player wins";
+    document.querySelector("#playerwin").innerText = playerWins;
+  } else if (playerChoice === "Paper" && computersChoice === "Scissors") {
+    computerWins++;
+    document.querySelector("#winner").innerText = "Computer wins";
+    document.querySelector("#computerwin").innerText = computerWins;
+  } else if (playerChoice === "Paper" && computersChoice === "Paper") {
+    draws++;
+    document.querySelector("#winner").innerText = "Draw";
+    document.querySelector("#game-draw").innerText = draws;
+  } else if (playerChoice === "Scissors" && computersChoice === "Rock") {
+    computerWins++;
+    document.querySelector("#winner").innerText = "Computer wins";
+    document.querySelector("#computerwin").innerText = computerWins;
+  } else if (playerChoice === "Scissors" && computersChoice === "Scissors") {
+    draws++;
+    document.querySelector("#winner").innerText = "Draw";
+    document.querySelector("#game-draw").innerText = draws;
+  } else if (playerChoice === "Scissors" && computersChoice === "Paper") {
+    playerWins++;
+    document.querySelector("#winner").innerText = "Player wins";
+    document.querySelector("#playerwin").innerText = playerWins;
+  }
+}
+
+function playerName() {
+  playersName = document.querySelector("#player-name-input").value;
+  if (playersName.length <= 10 && /^[A-Z][a-z]*/g.test(playersName)) {
+    document.querySelector("#name-restriction").style.display = "none";
+    document.querySelector("#player-position").innerText = playersName;
+    document.querySelector("#game-visibility").style.display = "block";
+  } else if (!/^[A-Z]/.test(playersName) && playersName.length > 10) {
+    document.querySelector("#name-restriction").style.display = "block";
+    document.querySelector("#name-restriction").innerText =
+      "Name cannot be more than 10 characther long! First letter of the username should be capitalised!";
+  } else if (/[^a-zA-Z]/.test(playersName)) {
+    document.querySelector("#name-restriction").style.display = "block";
+    document.querySelector("#name-restriction").innerText =
+      "Your player name can only contain letters!";
+  } else if (!/^[A-Z]/.test(playersName)) {
+    document.querySelector("#name-restriction").style.display = "block";
+    document.querySelector("#name-restriction").innerText =
+      "First letter of the username should be capitalised!";
+  } else if (playersName.length > 10) {
+    document.querySelector("#name-restriction").style.display = "block";
+    document.querySelector("#name-restriction").innerText =
+      "Name cannot be more than 10 characther long!";
+  }
+}
+
+let submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", playerName);
+
+let resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", reset);
+
+let playButton = document.querySelector("#play-button");
+playButton.addEventListener("click", selected);
+playButton.addEventListener("click", computerChoice);
+playButton.addEventListener("click", result);
 
 // while ((userResponse = true)) {
 //   let playerChoice = prompt("What is your choice?");
 
 //   function getWinner(playerMove, computerMove) {
 //     if (playerMove === "Rock" && computerMove === "Rock") {
-//       return "0";
 //       draws++;
+//       return "0";
 //     } else if (playerMove === "Rock" && computerMove === "Scissors") {
-//       return "1";
 //       playerWins++;
+//       return "1";
 //     } else if (playerMove === "Rock" && computerMove === "Paper") {
-//       return "-1";
 //       computerWins++;
+//       return "-1";
 //     } else if (playerMove === "Paper" && computerMove === "Rock") {
-//       return "1";
 //       playerWins++;
+//       return "1";
 //     } else if (playerMove === "Paper" && computerMove === "Scissors") {
-//       return "-1";
 //       computerWins++;
+//       return "-1";
 //     } else if (playerMove === "Paper" && computerMove === "Paper") {
-//       return "0";
 //       draw++;
+//       return "0";
 //     } else if (playerMove === "Scissors" && computerMove === "Rock") {
-//       return "-1";
 //       computerWins++;
+//       return "-1";
 //     } else if (playerMove === "Scissors" && computerMove === "Scissors") {
-//       return "0";
 //       draw++;
+//       return "0";
 //     } else if (playerMove === "Scissors" && computerMove === "Paper") {
-//       return "1";
 //       playerWins++;
+//       return "1";
 //     }
 //   }
 
 //   let computerChoiceArray = ["Rock", "Paper", "Scissors"];
 
+//   //Generate random number
 //   function computerMove() {
 //     return Math.floor(Math.random() * 3);
 //   }
+
 //   let computerChoice = computerChoiceArray[computerMove()];
 
 //   let result = getWinner(playerChoice, computerChoice);
